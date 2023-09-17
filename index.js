e6a98c16d9ec83fea1e21b45941252028143f5ca
dima-zharyy <zharyy.dimka@gmail.com>
2023-09-17 16:13:17 +0300

    fixed bug when a user-inviter removed from the group

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const TelegramBot = require('node-telegram-bot-api');
 require('dotenv').config();
 
 const { BOT_TOKEN } = process.env;
-const getWelcomeMessage = (userName) => `👋 Доброго дня, @${userName}.\nНапишіть з якої ви команди або будете видалені через 2 хвилини.`;
+const getWelcomeMessage = (userName, useMention) => `👋 Доброго дня, ${useMention ? '@' : ''}${userName}.\nНапишіть з якої ви команди або будете видалені через 2 хвилини.`;
 const TIMER_VALUE = 120000;
 
 const bot = new TelegramBot(BOT_TOKEN, { polling: true });
@@ -14,24 +14,28 @@ bot.on('message', (msg) => {
   const userId = msg.from.id;
 
   if (msg.new_chat_members) {
-    const newMember = msg.new_chat_members[0];
+    msg.new_chat_members.forEach((newMember) => {
+      const newMemberUserName = newMember.username ? newMember.username : `${newMember.first_name || ''} ${newMember.last_name || ''}`;
+      const newMemberUserId = newMember.id;
+      const useMention = !!newMember.username;
     
-    bot.sendMessage(chatId, getWelcomeMessage(newMember.username));
-
-    const timer = setTimeout(() => {
-      bot.getChatMember(chatId, userId)
-        .then((chatMember) => {
-          if (chatMember.status === 'member') {
-            bot.unbanChatMember(chatId, userId);
-            newUsersMap.delete(userId);
-          }
-        })
-        .catch((error) => {
-          console.error('Checking user status - unexpected error: ', error);
-        });
-    }, TIMER_VALUE);
-
-    newUsersMap.set(userId, timer);
+      bot.sendMessage(chatId, getWelcomeMessage(newMemberUserName, useMention));
+  
+      const timer = setTimeout(() => {
+        bot.getChatMember(chatId, newMemberUserId)
+          .then((chatMember) => {
+            if (chatMember.status === 'member') {
+              bot.unbanChatMember(chatId, newMemberUserId);
+              newUsersMap.delete(newMemberUserId);
+            }
+          })
+          .catch((error) => {
+            console.error('Checking user status - unexpected error: ', error);
+          });
+      }, TIMER_VALUE);
+  
+      newUsersMap.set(newMemberUserId, timer);
+    })
   } else if (msg.text && newUsersMap.has(userId)) {
     clearTimeout(newUsersMap.get(userId));
     newUsersMap.delete(userId);
